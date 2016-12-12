c2ae37bc3af4af93b36ea4736c2eee52c963a373
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-12-12 15:47:40 +0100

    update test config with dstore

--- tests/intern.js
+++ tests/intern.js
@@ -52,13 +52,15 @@ define({
     // Packages that should be registered with the loader in each testing environment
     packages: [
       { name: 'oe_dojo', location: '.' },
-      { name: 'dojo', location: 'dojo' }
+      { name: 'dojo', location: 'dojo' },
+      { name: 'dijit', location: 'dijit' },
+      { name: 'dstore', location: 'dstore' }
     ]
   },
 
   // A regular expression matching URLs to files that should not be included in code coverage analysis. Set to `true`
   // to completely disable code coverage.
-	excludeInstrumentation: /^(?:tests|node_modules|dojo|dijit)\//,
+	excludeInstrumentation: /^(?:tests|node_modules|dojo|dijit|dstore)\//,
 
   // Unit test suite(s) to run in each browser
   suites: [ 'tests/all' ],
