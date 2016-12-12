8ad165dc7e209ca9716d764d9952e30728a1f2d5
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-12-12 14:33:57 +0100

    remove firefox again, unclear which driver is used

--- tests/intern.js
+++ tests/intern.js
@@ -23,8 +23,8 @@ define({
   // and platformVersion; any other capabilities options specified for an environment will be copied as-is. Note that
   // browser and platform names, and version number formats, may differ between cloud testing systems.
   environments: [
-    { browserName: 'chrome' },
-    { browserName: 'firefox' }
+    { browserName: 'chrome' }
+    // { browserName: 'firefox' }
   ],
 
   // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
