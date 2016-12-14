8efa5cfb71de758edeabdb4b4fb64c0bf6c8e41f
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2016-12-14 10:55:52 +0100

    fix jshint error

--- tests/intern.js
+++ tests/intern.js
@@ -1,6 +1,7 @@
 // Learn more about configuring this file at <https://theintern.github.io/intern/#configuration>.
 // These default settings work OK for most people. The options that *must* be changed below are the packages, suites,
 // excludeInstrumentation, and (if you want functional tests) functionalSuites
+/* jshint unused:false */
 var dojoConfig = {
     locale: 'nl-be'
 };
