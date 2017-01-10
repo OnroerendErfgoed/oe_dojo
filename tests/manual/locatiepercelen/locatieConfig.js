7ec3f9c92049c75cc611ba408cab6710635f4574
Cedrik Vanderhaegen <cedrik.vanderhaegen@geosolutions.be>
2017-01-10 16:35:03 +0100

    remove proj4 amd flow

--- tests/manual/locatiepercelen/locatieConfig.js
+++ tests/manual/locatiepercelen/locatieConfig.js
@@ -9,7 +9,6 @@ var dojoConfig = {
   packages: [
     { name: 'jsts', location: '../jsts' },
     { name: 'oe_dojo', location: '..' },
-    { name: 'proj4', location: '../proj4', main: 'dist/proj4' },
     { name: 'olv319', location: '../olv319', main: 'ol-debug' }
   ]
 };
